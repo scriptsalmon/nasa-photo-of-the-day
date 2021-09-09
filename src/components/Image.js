3bd6a15bbeaf23cf1c2c16ab42d1857e30c6f2c7
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-09-09 16:54:34 -0700

    added styled-components

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const StyledImage = styled.div`
+// transform: scale(1) rotateZ(180deg);
+
+width: 60%;
+height: 20rem;
+
+img{
+    width:100%;
+    height:20rem;
+    object-fit:cover;
+    opacity:90%;
+    border:1px solid ${a => a.theme.white};
+  }
+img:hover{
+    opacity: 100%;
+  }
+
+`
+
+
+
 export default function Image (props) {
     const { data } = props;
     
     return (
-        <div className="Image">
+        <StyledImage>
             <h1>{data.title}</h1>
 
             <div className="Image-container">
                 <img src={data.url} alt={data.title} onClick={() => window.open(`${data.hdurl}`, "_blank")}/>
             </div>
 
-        </div>
+        </StyledImage>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {ThemeProvider} from 'styled-components';
+import theme from './theme';
 
 ReactDOM.render(
-    <App />
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
     , document.getElementById("root"));
